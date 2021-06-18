cf2e260efa87379e509a9e8651e5f37022c4d8df
MiguelAngelV1983 <82836220+MiguelAngelV1983@users.noreply.github.com>
2021-06-18 19:19:39 +0200

    Add files via upload

--- girl.py
+++ girl.py
@@ -1,41 +1,29 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "Untitled0.ipynb",
-      "provenance": [],
-      "collapsed_sections": [],
-      "authorship_tag": "ABX9TyPWQRO6iwP1SPrym0kgpt0+"
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "MbvHVdV11Drv"
-      },
-      "source": [
-        "import pygame\n",
-        "import sys\n",
-        "\n",
-        "pygame.init()\n",
-        "size = (800,600)\n",
-        "screen = pygame.display.set_mode(size)\n",
-        "\n",
-        "while True:\n",
-        "  for event in pygame.event.get():\n",
-        "    "
-      ],
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+import pygame
+import sys
+
+pygame.init()
+size = (800,600)
+image = "res/walking_animation.png"
+screen = pygame.display.set_mode(size)
+x = 400
+y = 200
+speed_x = 3
+speed_y = 3
+clock = pygame.time.Clock()
+
+
+while True:
+  for event in pygame.event.get():
+    print(event)
+    if event.type == pygame.QUIT:
+        pygame.exit()
+        sys.exit()
+  screen.fill(255, 255, 255)
+    
+  pygame.draw.rect(screen, image, (x,y,80,80))
+  pygame.display.flip()
+  if (x > 800 or x < 0):
+    speed_x*=-1  
+    x+=speed_x
+
+clock.tick(60)
