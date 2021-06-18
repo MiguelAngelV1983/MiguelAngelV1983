497f6f9b23787a1fb9ad00dd8c44b17f74ed2b09
MiguelAngelV1983 <82836220+MiguelAngelV1983@users.noreply.github.com>
2021-06-18 17:59:58 +0200

    Creado con Colaboratory

---
+++ girl.py
@@ -0,0 +1,41 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled0.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyPWQRO6iwP1SPrym0kgpt0+"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "MbvHVdV11Drv"
+      },
+      "source": [
+        "import pygame\n",
+        "import sys\n",
+        "\n",
+        "pygame.init()\n",
+        "size = (800,600)\n",
+        "screen = pygame.display.set_mode(size)\n",
+        "\n",
+        "while True:\n",
+        "  for event in pygame.event.get():\n",
+        "    "
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
